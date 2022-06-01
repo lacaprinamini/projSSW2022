c3816c6287aa5d42d699946c0f6265363ea66a81
lacaprinamini <frank.capria2000@gmail.com>
2022-06-01 12:10:33 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,6 +36,8 @@ export class AppComponent  {
   color: "";
   nominativo: string;
   chiave: any;
+  postiOccupatiPlatea: any;
+  postiOccupatiPalchi: any;
  
   constructor(private query: kvaasService) {}
       receiveKey($event) { 
@@ -45,9 +47,9 @@ export class AppComponent  {
         let postiDisponibiliPalchi=[];
         let postiDisponibiliPlatea=[];
         this.platea= new posti(7, 10, nomiPlatea, postiDisponibiliPlatea);
-        
         this.palchi=new posti(4, 6, nomiPalchi, postiDisponibiliPalchi);
-     
+        this.postiOccupatiPlatea=[{platea: "", nome: ""}]
+        this.postiOccupatiPlatea=[{palchi: "", nome: ""}]
         this.query.getData(this.chiave).subscribe({
       next: (x: any) => (console.log(JSON.parse(x))),
         error: err => console.error("Observer got an error: " + JSON.stringify(err))
@@ -59,13 +61,9 @@ export class AppComponent  {
       }
       
          impostaPlatea(posto: string){                  
-      for (var k in this.platea.postiDisponibili){
-        if(this.platea.postiDisponibili[k]==posto){
-          this.platea.postiNome[k]=this.nominativo;
-        }
-      }
-      
-      this.query.setData(this.platea, this.palchi).subscribe({
+    this.postiOccupatiPlatea.push({posto: posto, nome: this.nominativo})
+    this.postiOccupatiPlatea.splice(0, 1);
+      this.query.setData(this.postiOccupatiPlatea, this.postiOccupatiPalchi).subscribe({
         next: (x: any) => (console.log(x)),
         error: err => console.error("Observer got an error: " + JSON.stringify(err))
         });        
@@ -73,12 +71,9 @@ export class AppComponent  {
          }
          
          impostaPalchi(posto: string){                  
-          for (var k in this.palchi.postiDisponibili){
-            if(this.palchi.postiDisponibili[k]==posto){
-              this.palchi.postiNome[k]=this.nominativo;
-            }
-          }
-          this.query.setData(this.platea, this.palchi).subscribe({
+          this.postiOccupatiPalchi.push({posto: posto})
+          this.postiOccupatiPalchi.splice(0, 1);
+          this.query.setData(this.postiOccupatiPlatea, this.postiOccupatiPalchi).subscribe({
             next: (x: any) => (console.log(x)),
             error: err => console.error("Observer got an error: " + JSON.stringify(err))
             });        
